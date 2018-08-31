aca04447ea069019ef381adb6a115e3496f14c43
akiyamka <akiyamka@gmail.com>
2018-08-31 17:44:34 +0300

    first try

--- index2.js
+++ index2.js
@@ -9,18 +9,16 @@ function startApp() {
       super();
       this.tiles = tiles;
       this.maxTiles = tiles.length - 1;
-      this.canvas = wire()`<canvas id="canvas" width=256 height=256></canvas>`
-      this.topImage
-      this.ctx = null;
-      this.imagesStore = [];
+      this.topImageLayer = wire()`<img src="./frames/1.png" alt=""/ style="position: absolute">`;
+      this.bottomImageLayer = wire()`<img src="./frames/2.png" alt=""/>`
       this.fadeFactor = 0; // incriment from 0 -> 100 at animation
+      this.imagesStore = []
       this.setState({
         currentTileIndex: 0,
       });
     }
 
     onconnected() {
-      this.ctx = this.canvas.getContext("2d");
       this.loadAllImages().then(() => {
         console.log('images loaded', this.imagesStore)
       })
@@ -34,12 +32,12 @@ function startApp() {
     }
 
     get currentImage() {
-      return this.imagesStore[this.state.currentTileIndex].imgEl;
+      return this.imagesStore[this.state.currentTileIndex].src;
     }
 
     get futureImage() {
       const futureIndex = Number(this.state.currentTileIndex) + 1;
-      return this.imagesStore[futureIndex > this.maxTiles ? 0 : futureIndex].imgEl;
+      return this.imagesStore[futureIndex > this.maxTiles ? 0 : futureIndex].src;
     }
 
     loadImage(url) {
@@ -73,40 +71,30 @@ function startApp() {
 
       return Promise.all(loadQueue).then(images => {
         this.imagesStore = images || [];
-        this.draw(this.imagesStore[0].imgEl);
       });
 
     }
 
-    draw(img, opacity = 1) {
-      this.ctx.save();
-      this.ctx.globalAlpha = opacity;
-      this.ctx.drawImage(img, 0, 0);
-      this.ctx.restore();
+    draw(img, animationName) {
+      img.classList.remove(animationName);
+      void img.offsetWidth;
+      img.classList.add(animationName);
     }
 
     animateFade() {
-      const fromImage = this.currentImage;
-      const toImage = this.futureImage;
-
-
-      if (this.fadeFactor > 100) {
-        this.setState(state => {
-          const nextTile = Number(state.currentTileIndex) + 1;
-          return {
-            ...state,
-            currentTileIndex: nextTile > this.maxTiles ? 0 : nextTile,
-          }
-        });
-        this.fadeFactor = 0;
-        return;
-      }
-      requestAnimationFrame(this.animateFade.bind(this));
-
-      this.draw(fromImage, (1 - this.fadeFactor / 100));
-      this.draw(toImage, this.fadeFactor / 100);
-
-      this.fadeFactor++;
+      this.topImageLayer.src = this.currentImage;
+      this.bottomImageLayer.src = this.futureImage;
+      console.log('TCL: Counter -> animateFade -> topImageLayer.src', this.topImageLayer.src);
+      this.draw(this.bottomImageLayer, 'anim-in')
+      setTimeout(this.draw.bind(this, this.topImageLayer, 'anim-out'), 3000)
+      // this.draw(this.topImageLayer, 'anim-out')
+      this.setState(state => {
+        const newIndex = state.currentTileIndex + 1;
+        return {
+          ...state,
+          currentTileIndex: newIndex > this.maxTiles ? 0 : newIndex
+        }
+      });
     }
 
     onchange(event) {
@@ -131,24 +119,26 @@ function startApp() {
       if (this.state.isPaused) {
         window.clearInterval(this.animation);
       } else {
-        this.animation = window.setInterval(this.animateFade.bind(this), 3000);
+        this.animation = window.setInterval(this.animateFade.bind(this), 6000);
       }
     }
 
     render() {
       return this.html`
-      <div class="view" onconnected=${this}>
-        ${this.canvas}
-      </div>
-      <input type="range" name="timeline" id="timeline" min="0" max=${this.maxTiles} value=${this.state.currentTileIndex} onchange=${this} />
-      <div>
-        Frame: ${this.state.currentTileIndex}
-      </div>
-      <button style="margin: 18px; padding: 8px 12px" onclick="${this}"> ${this.state.isPaused ? 'Start' : 'Stop'}</button>`
+        <hr>
+        <div class="view" onconnected=${this}>
+          ${this.topImageLayer}
+          ${this.bottomImageLayer}
+        </div>
+        <input type="range" name="timeline" id="timeline" min="0" max=${this.maxTiles} value=${this.state.currentTileIndex} onchange=${this} />
+        <div>
+          Frame: ${this.state.currentTileIndex}
+        </div>
+        <button style="margin: 18px; padding: 8px 12px" onclick="${this}"> ${this.state.isPaused ? 'Start' : 'Stop'}</button>`
     }
   }
 
-  hyper(document.querySelector('#app'))`${new Counter([
+  hyper(document.querySelector('#app2'))`${new Counter([
     'frames/1.png',
     'frames/2.png',
     'frames/3.png',
